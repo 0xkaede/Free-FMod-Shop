31f83ab91818778524eb631cd8643949a8f87356
kaede <masseyj218@gmail.com>
2023-05-08 01:05:40 +0100

    Init

---
+++ FreeFModShop/FreeFModShop/Models.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+
+namespace FreeFModShop
+{
+    public class PostItem
+    {
+        [JsonProperty("offerId")]
+        public string OfferId { get; set; }
+
+        [JsonProperty("purchaseQuantity")]
+        public int PurchaseQuantity { get; set; } = 1;
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; } = "MtxCurrency";
+
+        [JsonProperty("currencySubType")]
+        public string CurrencySubType { get; set; } = string.Empty;
+
+        [JsonProperty("expectedTotalPrice")]
+        public int ExpectedTotalPrice { get; set; } = 0;
+
+        [JsonProperty("gameContext")]
+        public string GameContext { get; set; } = string.Empty;
+    }
+
+    public class CatalogEntry
+    {
+        [JsonProperty("devName")]
+        public string DevName { get; set; }
+
+        [JsonProperty("offerId")]
+        public string OfferId { get; set; }
+
+        [JsonProperty("fulfillmentIds")]
+        public List<object> FulfillmentIds { get; set; }
+
+        [JsonProperty("dailyLimit")]
+        public int DailyLimit { get; set; }
+
+        [JsonProperty("weeklyLimit")]
+        public int WeeklyLimit { get; set; }
+
+        [JsonProperty("monthlyLimit")]
+        public int MonthlyLimit { get; set; }
+
+        [JsonProperty("categories")]
+        public List<string> Categories { get; set; }
+
+        [JsonProperty("prices")]
+        public List<Price> Prices { get; set; }
+
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+
+        [JsonProperty("matchFilter")]
+        public string MatchFilter { get; set; }
+
+        [JsonProperty("filterWeight")]
+        public int FilterWeight { get; set; }
+
+        [JsonProperty("appStoreId")]
+        public List<object> AppStoreId { get; set; }
+
+        [JsonProperty("requirements")]
+        public List<Requirement> Requirements { get; set; }
+
+        [JsonProperty("offerType")]
+        public string OfferType { get; set; }
+
+        [JsonProperty("giftInfo")]
+        public GiftInfo GiftInfo { get; set; }
+
+        [JsonProperty("refundable")]
+        public bool Refundable { get; set; }
+
+        [JsonProperty("metaInfo")]
+        public List<object> MetaInfo { get; set; }
+
+        [JsonProperty("displayAssetPath")]
+        public string DisplayAssetPath { get; set; }
+
+        [JsonProperty("itemGrants")]
+        public List<ItemGrant> ItemGrants { get; set; }
+
+        [JsonProperty("sortPriority")]
+        public int SortPriority { get; set; }
+
+        [JsonProperty("catalogGroupPriority")]
+        public int CatalogGroupPriority { get; set; }
+    }
+
+    public class GiftInfo
+    {
+    }
+
+    public class ItemGrant
+    {
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("templateId")]
+        public string TemplateId { get; set; }
+    }
+
+    public class Meta
+    {
+    }
+
+    public class Price
+    {
+        [JsonProperty("currencyType")]
+        public string CurrencyType { get; set; }
+
+        [JsonProperty("currencySubType")]
+        public string CurrencySubType { get; set; }
+
+        [JsonProperty("regularPrice")]
+        public int RegularPrice { get; set; }
+
+        [JsonProperty("finalPrice")]
+        public int FinalPrice { get; set; }
+
+        [JsonProperty("saleExpiration")]
+        public DateTime SaleExpiration { get; set; }
+
+        [JsonProperty("basePrice")]
+        public int BasePrice { get; set; }
+    }
+
+    public class Requirement
+    {
+        [JsonProperty("requirementType")]
+        public string RequirementType { get; set; }
+
+        [JsonProperty("requiredId")]
+        public string RequiredId { get; set; }
+
+        [JsonProperty("minQuantity")]
+        public int MinQuantity { get; set; }
+    }
+
+    public class StoreFrontRoot
+    {
+        [JsonProperty("refreshIntervalHrs")]
+        public int RefreshIntervalHrs { get; set; }
+
+        [JsonProperty("dailyPurchaseHrs")]
+        public int DailyPurchaseHrs { get; set; }
+
+        [JsonProperty("expiration")]
+        public DateTime Expiration { get; set; }
+
+        [JsonProperty("storefronts")]
+        public List<Storefront> Storefronts { get; set; }
+    }
+
+    public class Storefront
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("catalogEntries")]
+        public List<CatalogEntry> CatalogEntries { get; set; }
+    }
+}
---
+++ FreeFModShop/FreeFModShop/Program.cs
@@ -0,0 +1,95 @@
+﻿//
+// Created by 0xkaede
+//
+
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace FreeFModShop
+{
+    internal class Program
+    {
+        private static readonly string ID = "User ID Here";
+        private static readonly string Bearer = "Bearer Here";
+
+        public static async Task Main(string[] args)
+        {
+            Console.Title = "Free Fmod items by 0xkaede";
+            Console.WriteLine("Created by 0xkaede thank you for using! Be warned you will go into minus vbucks!");
+
+            foreach (var id in await GetFmodOfferIds())
+                await PurchaseItem(id);
+
+            Console.WriteLine("Done!");
+            Console.Read();
+        }
+
+        public async static Task<List<string>> GetFmodOfferIds()
+        {
+            var Idlist = new List<string>();
+
+            try
+            {
+                var data = JsonConvert.DeserializeObject<StoreFrontRoot>
+                    (await new HttpClient().GetStringAsync(new Uri("https://api.fmod.dev/fortnite/api/storefront/v2/catalog")));
+
+                if (data is null)
+                    return Idlist;
+
+                foreach (var storeFront in data.Storefronts)
+                {
+                    if (storeFront.Name is "BRSeasonStorefront")
+                        continue;
+
+                    foreach (var catalogEntries in storeFront.CatalogEntries)
+                        Idlist.Add(catalogEntries.OfferId);
+                }
+
+                return Idlist;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error when getting Shop Offsets, Message: {ex.Message}");
+                Console.Read();
+                return Idlist;
+            }
+        }
+
+        public async static Task PurchaseItem(string offerId)
+        {
+            var item = new PostItem
+            {
+                OfferId = offerId,
+                PurchaseQuantity = 1,
+                Currency = "MtxCurrency",
+                CurrencySubType = string.Empty,
+                ExpectedTotalPrice = 0,
+                GameContext = string.Empty
+            };
+
+            var options = new RestClientOptions("https://api.fmod.dev")
+            {
+                MaxTimeout = -1,
+                UserAgent = " Fortnite/++Fortnite+Release-8.51-CL-6165369 Windows/10.0.19045.1.768.64bit",
+            };
+
+            var client = new RestClient(options);
+            var request = new RestRequest($"/fortnite/api/game/v2/profile/{ID}/client/PurchaseCatalogEntry?profileId=common_core&rvn=17/fortnite/api/game/v2/profile/b5w05lhys5963f4vjvg3cyyvf77vfuni/client/PurchaseCatalogEntry?profileId=common_core&rvn=17", Method.Post)
+                .AddHeader("Content-Type", "application/json")
+                .AddHeader("X-EpicGames-AnalyticsSessionId", "{1B94E773-4A2A-4D03-5F1D-F79261A8204F}")
+                .AddHeader("X-EpicGames-GameSessionId", "FRONTEND-14B5C7BF4BCE0246CA7E52BA7D3FE755")
+                .AddHeader("X-EpicGames-ProfileRevisions", "[{\"profileId\":\"common_public\",\"clientCommandRevision\":0},{\"profileId\":\"common_core\",\"clientCommandRevision\":17},{\"profileId\":\"athena\",\"clientCommandRevision\":29}]")
+                .AddHeader("X-Epic-Correlation-ID", "FN-5OXtiiYSTEWn27WpNcVAGg")
+                .AddHeader("Authorization", $"bearer {Bearer}")
+                .AddStringBody(JsonConvert.SerializeObject(item), DataFormat.Json);
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.StatusCode is System.Net.HttpStatusCode.OK)
+                Console.WriteLine($"{offerId} was purchaced.");
+            else
+                Console.WriteLine($"Error when trying to Purchase {offerId}. CODE: {response.StatusCode}");
+
+        }
+    }
+}
